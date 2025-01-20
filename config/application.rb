bc63cc9961839c5312302c5d27178121ac7fbbf3
JuanFuent.es <input@juanfuent.es>
2025-01-19 20:06:30 -0600

    Update available langs

--- config/application.rb
+++ config/application.rb
@@ -21,7 +21,10 @@ module Colmena
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    # config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Mexico City'
+    
+    I18n.available_locales = [:es, :en]
+    I18n.default_locale = :es
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
