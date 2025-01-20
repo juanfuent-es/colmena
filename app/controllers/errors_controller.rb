21d2b74acef54dc0fc13dc26ddb407fd41752908
JuanFuent.es <input@juanfuent.es>
2025-01-20 14:44:24 -0600

    Add Custom Error Responses

---
+++ app/controllers/errors_controller.rb
@@ -0,0 +1,28 @@
+class ErrorsController < ApplicationController
+
+  skip_after_action :verify_same_origin_request
+  
+  # https://guides.rubyonrails.org/v4.2.0/action_controller_overview.html
+  layout false
+  
+  def not_found
+    render status: :not_found
+  end
+  
+  def script_not_found
+    send_file 'public/script-not-found.js', type: 'text/javascript', disposition: 'inline', status: :not_found
+  end
+  
+  def unprocessable_entity
+    render status: :unprocessable_entity
+  end
+  
+  def server_error
+    render status: :internal_server_error
+  end
+
+  def image_not_found
+  	send_file 'public/img-not-found.png', type: 'image/png', disposition: 'inline'
+  end
+
+end
