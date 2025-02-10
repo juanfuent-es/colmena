357d9b38176726efc8c745bb56e7b7779884e92f
JuanFuent.es <input@juanfuent.es>
2025-02-10 08:51:13 -0600

    Update Layout Partials

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,32 +1,32 @@
 module.exports = {
-    important: false,
-    // darkMode: false,
-    content: [
-      './app/views/**/*.html.erb',
-      './app/helpers/**/*.rb',
-      './app/assets/stylesheets/**/*.{css,scss}',
-      './app/javascript/**/*.js*'
-    ],
-    plugins: [
-      require("tailwindcss"),
-      require("@tailwindcss/forms")({
-        strategy: 'base', // only generalote global styles
-        // strategy: 'class', // only generate classes
-      }),
-      require('postcss-import'),
-      require('postcss-preset-env')({
-        autoprefixer: {
-          flexbox: 'no-2009'
-        },
-        stage: 3
-      })
-    ],
-    theme: { // @see: https://tailwindcss.com/docs/customizing-colors
-      extend: {},
-      colors: { // https://materialpalettes.com/
-        current: "currentColor",
-        black: '#000000',
-        white: '#FFFFFF',
-      }
+  important: false,
+  // darkMode: false,
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/tailwind/**/*.{css}',
+    './app/javascript/**/*.js*'
+  ],
+  plugins: [
+    require("tailwindcss"),
+    require("@tailwindcss/forms")({
+      strategy: 'base', // only generalote global styles
+      // strategy: 'class', // only generate classes
+    }),
+    require('postcss-import'),
+    require('postcss-preset-env')({
+      autoprefixer: {
+        flexbox: 'no-2009'
+      },
+      stage: 3
+    })
+  ],
+  theme: { // @see: https://tailwindcss.com/docs/customizing-colors
+    extend: {},
+    colors: { // https://materialpalettes.com/
+      current: "currentColor",
+      black: '#000000',
+      white: '#FFFFFF',
     }
   }
+}
