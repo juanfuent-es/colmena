d64d33a58323a644010efa896fde5d65d035464e
JuanFuent.es <input@juanfuent.es>
2025-03-07 22:59:19 -0600

    Update Theme / Colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,8 +29,18 @@ module.exports = {
     },
     colors: { // https://materialpalettes.com/
       current: "currentColor",
-      black: '#000000',
-      white: '#FFFFFF',
+      aqua: {
+        DEFAULT: '#00FFFF',
+        100: "#ccffff",
+        200: "#99ffff",
+        300: "#66ffff",
+        400: "#33ffff",
+        500: "#00ffff",
+        600: "#00cccc",
+        700: "#009999",
+        800: "#006666",
+        900: "#003333"
+},
     }
   }
 }
