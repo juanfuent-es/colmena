76c8696a123a5bd4195dc239646ec98d362cc58f
JuanFuent.es <input@juanfuent.es>
2025-03-19 14:34:34 -0600

    Update Admin Core

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,34 @@
 class ApplicationController < ActionController::Base
-  # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
-  # allow_browser versions: :modern
+
+	rescue_from ActiveRecord::RecordNotFound, :with => :render_not_found
+	after_action :set_vary_header
+
+	def index
+		render template: 'layouts/application'
+	end
+
+  # ¿Hay un administrador logueado?
+  def authenticate_admin!
+		if signed_in?
+      redirect_to root_path if !current_user.admin?
+		else
+			authenticate_user!
+		end
+  end
+
+	def render_not_found
+    @page = Page.find_by_slug("404")
+    render template: 'errors/not_found', status: :not_found, layout: 'errors'
+	end
+
+  private
+
+	def locale_valid?(locale)
+		I18n.available_locales.map(&:to_s).include?(locale)
+	end
+
+    def set_vary_header
+		response.headers["Vary"] = "accept" if request.xhr?
+	end
+
 end
