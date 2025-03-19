bbe6e0bb21585ed36a2dbeea85363953da171f5d
JuanFuent.es <input@juanfuent.es>
2025-03-19 14:55:42 -0600

    Update scaffolding markups

--- db/migrate/20250319190946_create_quotations.rb
+++ db/migrate/20250319190946_create_quotations.rb
@@ -1,15 +1,17 @@
 class CreateQuotations < ActiveRecord::Migration[7.2]
   def change
-    create_table :quotations do |t|
-      t.string :client
-      t.string :logo
-      t.string :project
-      t.string :description
-      t.string :title
-      t.string :slug
-      t.string :footer
-      t.integer :weeks
-      t.integer :total
+
+    enable_extension 'pgcrypto' unless extension_enabled?('pgcrypto')
+    create_table :quotations, id: :uuid do |t|
+      t.string :client,       default: "", null: false
+      t.string :logo,         default: "", null: false
+      t.string :project,      default: ""
+      t.string :description,  default: ""
+      t.string :title,        default: ""
+      t.string :slug,         default: ""
+      t.string :footer,       default: ""
+      t.integer :weeks,       default: 0
+      t.integer :total,       default: 0
 
       t.timestamps
     end
