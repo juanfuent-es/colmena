13246a0b220a739a3c703b562cfdbc02982166a9
JuanFuent.es <input@juanfuent.es>
2025-06-01 16:42:01 -0600

    Update LinksMarkup

--- db/migrate/20250418033317_create_images.rb
+++ db/migrate/20250418033317_create_images.rb
@@ -1,13 +1,11 @@
 class CreateImages < ActiveRecord::Migration[7.2]
   def change
-    # images optimizadas para frontend, jpg, webp
-    # no hace falta guardar la extensión, ya quet todas se procesan en jpg y webp
+
     create_table :images do |t|
       t.string :file,       null: false, default: ""
-      #
       t.integer :width,     null: false, default: 0
       t.integer :height,    null: false, default: 0
-      t.string :orientation,null: false, default: "landscape", comment: "landscape|portrait"
+      t.string :orientation,null: false, default: "landscape", comment: "landscape|portrait" # no se usa
       t.boolean :processed, default: false
       t.string :status,     null: false, default: "empty", comment: "empty|processing|ready"
 
